b407c29712600e40bde90533012c91f08fcb69d0
Leslie Thompson <lcrthompson@gmail.com>
2020-01-28 14:44:39 -0500

    server update

--- server.js
+++ server.js
@@ -15,18 +15,4 @@ server.use(express.json());
 
 adminRoute(server);
 
-// server.get("/", (req, res) => {
-//   res.send(
-//     "<h1> Welcome to Project Motivation Backend!</h1>" +
-//       "<br/>" +
-//       "Built by: Leslie Thompson" +
-//       "<br />" +
-//       "<br />" +
-//       "GitHub: https://github.com/LCRT215/Project-Motivation-BE" +
-//       "<br />" +
-//       "<br />" +
-//       "Deployed Backend: https://motivation-backend.herokuapp.com/"
-//   );
-// });
-
 module.exports = server;
