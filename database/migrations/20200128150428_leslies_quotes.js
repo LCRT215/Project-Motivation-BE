6c52ecebfd7477cf51c05d4c48963038fbd8047c
Leslie Thompson <lcrthompson@gmail.com>
2020-02-11 18:41:57 -0500

    post new quote

--- database/migrations/20200128150428_leslies_quotes.js
+++ database/migrations/20200128150428_leslies_quotes.js
@@ -1,12 +1,15 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("lesliesQuotes", function(tbl) {
     tbl.increments();
-    tbl.text("quote", 500).notNullable();
+    tbl
+      .text("quote", 500)
+      .notNullable()
+      .unique();
     tbl.string("author", 128).notNullable();
-    table.timestamp("created_at").defaultTo(knex.fn.now());
+    tbl.timestamp("created_at").defaultTo(knex.fn.now());
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExist("lesliesQuotes");
+  return knex.schema.dropTableIfExists("lesliesQuotes");
 };
